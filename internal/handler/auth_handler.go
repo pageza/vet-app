9e58de571b5d5311fb70a243f294055950019e3e
Zach <zap.freelance@gmail.com>
2023-11-08 22:32:02 +0000

    added dummy logic to routes handler to test

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -3,6 +3,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 	// Import other necessary packages
 )
@@ -12,29 +13,47 @@ type AuthHandler struct {
 	// Add fields for dependencies, like a service layer or config
 }
 
+// dummyResponse is a helper function to send a dummy JSON response
+func dummyResponse(w http.ResponseWriter, route string) {
+	response := map[string]string{"route": route}
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Error creating JSON response", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+}
+
 // NewAuthHandler creates a new AuthHandler with the necessary dependencies
 func NewAuthHandler( /* pass dependencies here */ ) *AuthHandler {
 	return &AuthHandler{
 		// Initialize fields with the passed dependencies
+
 	}
 }
 
 // Login handles the user login
 func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 	// Your login logic here
+	dummyResponse(w, "/login")
 }
 
 // Register handles new user registration
 func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
 	// Your registration logic here
+	dummyResponse(w, "/register")
 }
 
 // Logout handles user logout
 func (h *AuthHandler) Logout(w http.ResponseWriter, r *http.Request) {
 	// Your logout logic here
+	dummyResponse(w, "/logout")
 }
 
 // Profile handles fetching the user profile
 func (h *AuthHandler) Profile(w http.ResponseWriter, r *http.Request) {
 	// Your profile fetching logic here
+	dummyResponse(w, "/profile")
 }
