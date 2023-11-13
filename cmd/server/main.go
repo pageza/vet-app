d1aeff2b572ed27eea75fd4861bdd8dc1e22b91e
Zach <zap.freelance@gmail.com>
2023-11-13 20:27:10 +0000

    straightening out nil pointer reference error

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,22 +7,21 @@ import (
 	"github.com/pageza/vet-app/internal/database"
 	"github.com/pageza/vet-app/internal/handler"
 	"github.com/pageza/vet-app/internal/user"
-	"gorm.io/gorm"
 )
 
 func main() {
+	// Initialize your database connection.
+	database.Init()
+	db := database.DB
 
-	// Initialize your database connection (db).
-	var db *gorm.DB
-
-	// Set up the user repository and service.
+	// Set up the user repository and service with the database connection.
 	userRepo := database.NewUserRepository(db)
 	userService := user.NewUserService(userRepo)
 
-	// Initialize the auth handler
+	// Initialize the auth handler without passing userService.
 	authHandler := handler.NewAuthHandler()
 
-	// Setup the routes with the auth handler and user service
+	// Setup the routes with the auth handler and userService.
 	mux := handler.SetupRoutes(authHandler, userService)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
